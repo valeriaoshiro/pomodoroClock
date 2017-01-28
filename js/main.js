6341a41dc0ed4a6c33ebc8adba98d2711edc02e2
valeriaoshiro <valeriaoshiro19@gmail.com>
2017-01-28 13:43:54 -0800

    Start, Stop, Reset buttons are working

--- js/main.js
+++ js/main.js
@@ -1,1 +1,56 @@
-alert('hi');
+$(document).ready(function(){
+	var $breakDisplay = $('.break-display');
+	var $workDisplay = $('.work-display');
+	var isNew = true;
+
+	$('.break .minus').on('click', function(){
+		var holder = Number($breakDisplay.text());
+		holder--;
+		if(holder < 0) {
+			holder = 0;
+		}
+		$breakDisplay.text(holder);
+	});
+	$('.break .plus').on('click', function(){
+		var holder = Number($breakDisplay.text());
+		holder++;
+		$breakDisplay.text(holder);
+	});
+	$('.work .minus').on('click', function(){
+		var holder = Number($workDisplay.text());
+		holder--;
+		if(holder < 0) {
+			holder = 0;
+		}
+		$workDisplay.text(holder);
+	});
+	$('.work .plus').on('click', function(){
+		var holder = Number($workDisplay.text());
+		holder++;
+		$workDisplay.text(holder);
+	});
+	$('.start').on('click', function(){
+		if(isNew) {
+			$('h1').countdown({until: +Number($workDisplay.text()), format: 'MS', compact: true, onExpiry: workFinish});
+			isNew = false;		
+		} else {
+			$('h1').countdown('resume');
+		}
+	});
+	$('.stop').on('click', function() { 
+	    $('h1').countdown('pause'); 
+	});
+	$('.reset').on('click', function(){
+		$('h1').countdown('destroy');
+		$('h1').countdown({until: +Number($workDisplay.text()), format: 'MS', compact: true, onExpiry: workFinish});
+		isNew = false;
+	});
+
+	function workFinish(){
+		alert('finish');
+		isNew = true;
+	}
+	
+});
+
+// +Number($workDisplay.text())+'m'
